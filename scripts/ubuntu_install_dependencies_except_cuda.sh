59ab4647777525133ba2547a824f607e4b55c1bd
Bart van Blokland <bartvblokl@gmail.com>
2020-07-09 01:10:47 +0200

    Added missing dependencies

--- scripts/ubuntu_install_dependencies_except_cuda.sh
+++ scripts/ubuntu_install_dependencies_except_cuda.sh
@@ -1,3 +1,3 @@
 sudo apt install cmake python3 python3-pip libpcl-dev g++-7 gcc-7
 
-sudo pip3 install simple-term-menu xlwt numpy matplotlib pillow 
+sudo pip3 install simple-term-menu xlwt xlrd numpy matplotlib pillow PyQt5
