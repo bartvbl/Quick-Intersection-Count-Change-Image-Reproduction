4a4bb584782ff7f81e092bee9d89ac935f5caaad
bartvbl <bartvblokl@gmail.com>
2020-08-06 00:42:32 +0200

    Fixed wrong byte offset when writing QUICCI dump files

--- src/libSpinImage/src/spinImage/utilities/dumpers/rawDescriptorDumper.cpp
+++ src/libSpinImage/src/spinImage/utilities/dumpers/rawDescriptorDumper.cpp
@@ -9,17 +9,17 @@ void SpinImage::dump::raw::descriptors(
     const unsigned int imageWidthPixels = spinImageWidthPixels;
 
     size_t imageBlockSize = images.imageCount * sizeof(QuiccImage);
-    size_t outFileBufferSize = 5 + sizeof(size_t) + sizeof(unsigned int) + 2 * imageBlockSize;
+    size_t outFileBufferSize = 4 + sizeof(size_t) + sizeof(unsigned int) + 2 * imageBlockSize;
     char* outFileBuffer = new char[outFileBufferSize];
     
     const std::string header = "QUIC";
     
     std::copy(header.begin(), header.end(), outFileBuffer);
-    outFileBuffer[4] = 0;
-    *reinterpret_cast<size_t*>(outFileBuffer + 5) = images.imageCount;
-    *reinterpret_cast<unsigned int*>(outFileBuffer + 5 + sizeof(size_t)) = imageWidthPixels;
+
+    *reinterpret_cast<size_t*>(outFileBuffer + 4) = images.imageCount;
+    *reinterpret_cast<unsigned int*>(outFileBuffer + 4 + sizeof(size_t)) = imageWidthPixels;
     std::copy(images.images, images.images + images.imageCount,
-            reinterpret_cast<QuiccImage*>(outFileBuffer + 5 + sizeof(size_t) + sizeof(unsigned int)));
+            reinterpret_cast<QuiccImage*>(outFileBuffer + 4 + sizeof(size_t) + sizeof(unsigned int)));
 
     SpinImage::utilities::writeCompressedFile(outFileBuffer, outFileBufferSize, outputDumpFile);
     
